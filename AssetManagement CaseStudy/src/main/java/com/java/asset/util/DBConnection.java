0b1953bc689171756082bf49fd6b41ff64c055cd
SubhaShri016 <137605466+SubhaShri016@users.noreply.github.com>
2025-04-12 11:46:19 +0530

    Update DBConnection.java

--- AssetManagement CaseStudy/src/main/java/com/java/asset/util/DBConnection.java
+++ AssetManagement CaseStudy/src/main/java/com/java/asset/util/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.Connection;
 public class DBConnection {
 
     public static Connection getConnection() {
-        // Directly return a new connection every time it's requested
+       
         return DBConnUtil.getConnection();
     }
 }
