4d1da108605b2bf0fb1895e90758c915b356c845
SubhaShri016 <137605466+SubhaShri016@users.noreply.github.com>
2025-04-12 11:12:56 +0530

    Update AssetNotFoundException.java

--- AssetManagement CaseStudy/src/main/java/com/java/asset/MyExceptions/AssetNotFoundException.java
+++ AssetManagement CaseStudy/src/main/java/com/java/asset/MyExceptions/AssetNotFoundException.java
@@ -2,7 +2,7 @@ package com.java.asset.MyExceptions;
 
 public class AssetNotFoundException extends Exception {
 
-    private static final long serialVersionUID = 1L; // Added serialVersionUID
+    private static final long serialVersionUID = 1L; 
 
     public AssetNotFoundException(String message) {
         super(message);
